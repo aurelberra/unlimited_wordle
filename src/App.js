092681bf19d0c381b6f21cdb98892507b18f955a
VyomGarg47 <vyomgarg.official@gmail.com>
2022-03-14 21:14:55 +0530

    Added grid focus

--- src/App.js
+++ src/App.js
@@ -32,25 +32,65 @@ class App extends Component {
     };
   }
 
+  async handleKeyDown(e, row, col) {
+    if (e.keyCode === 8) {
+      if (e.target.value.length === 0) {
+        var prevrow = await row;
+        var prevcol = await col;
+        if (prevcol > 0) {
+          prevcol--;
+        } else {
+          if (prevrow > 0) {
+            prevrow--;
+            prevcol = 4;
+          }
+        }
+        if (prevrow >= 0 && prevcol >= 0) {
+          var temp_matrix = await this.state.matrix;
+          if (row !== 0 || col !== 0) {
+            temp_matrix[row][col] = await true;
+          }
+          const prevfield = await document.querySelector(
+            `input[name=field-${prevrow}${prevcol}]`
+          );
+          console.log("prevrow" + prevrow);
+          console.log("prevcol" + prevcol);
+          if (row !== 0 || col !== 0) {
+            await this.setState({
+              matrix: temp_matrix,
+            });
+          }
+
+          if (prevfield !== null) {
+            prevfield.value = "";
+            prevfield.focus();
+          }
+        }
+      }
+    }
+  }
+
   async handlechange(e, row, col) {
     console.log(row);
     console.log(col);
     var curr = e.target.value;
-    if (curr.length == 1) {
-      var temp_matrix = this.state.matrix;
+    if (curr.length === 1) {
+      var temp_matrix = await this.state.matrix;
       var nextrow = row;
       var nextcol = col;
       if (col <= 3) {
         nextcol = col + 1;
       } else {
-        nextrow = row + 1;
-        nextcol = 0;
+        if (row <= 4) {
+          nextrow = row + 1;
+          nextcol = 0;
+        }
       }
       if (nextrow <= 5 && nextcol <= 4) {
         temp_matrix[nextrow][nextcol] = false;
         console.log(nextrow);
         console.log(nextcol);
-        const nextfield = document.querySelector(
+        const nextfield = await document.querySelector(
           `input[name=field-${nextrow}${nextcol}]`
         );
 
@@ -66,8 +106,10 @@ class App extends Component {
     }
   }
 
-  componentDidMount() {
-    const nextfield = document.querySelector(`input[name=field-${0}${0}]`);
+  async componentDidMount() {
+    const nextfield = await document.querySelector(
+      `input[name=field-${0}${0}]`
+    );
     if (nextfield !== null) {
       nextfield.focus();
     }
@@ -96,6 +138,9 @@ class App extends Component {
                   {...input_props_2}
                   //ref={this[`inputRef${index + 1}1`]}
                   disabled={this.state.matrix[index][0]}
+                  onKeyDown={(e) => {
+                    this.handleKeyDown(e, index, 0);
+                  }}
                   onChange={(e) => {
                     this.handlechange(e, index, 0);
                   }}
@@ -104,6 +149,9 @@ class App extends Component {
                   name={`field-${index}${1}`}
                   {...input_props_2}
                   //ref={this[`inputRef${index + 1}2`]}
+                  onKeyDown={(e) => {
+                    this.handleKeyDown(e, index, 1);
+                  }}
                   disabled={this.state.matrix[index][1]}
                   onChange={(e) => {
                     this.handlechange(e, index, 1);
@@ -113,6 +161,9 @@ class App extends Component {
                   name={`field-${index}${2}`}
                   {...input_props_2}
                   //ref={this[`inputRef${index + 1}3`]}
+                  onKeyDown={(e) => {
+                    this.handleKeyDown(e, index, 2);
+                  }}
                   disabled={this.state.matrix[index][2]}
                   onChange={(e) => {
                     this.handlechange(e, index, 2);
@@ -122,6 +173,9 @@ class App extends Component {
                   name={`field-${index}${3}`}
                   {...input_props_2}
                   //ref={this[`inputRef${index + 1}4`]}
+                  onKeyDown={(e) => {
+                    this.handleKeyDown(e, index, 3);
+                  }}
                   disabled={this.state.matrix[index][3]}
                   onChange={(e) => {
                     this.handlechange(e, index, 3);
@@ -131,6 +185,9 @@ class App extends Component {
                   name={`field-${index}${4}`}
                   {...input_props_2}
                   //ref={this[`inputRef${index}5`]}
+                  onKeyDown={(e) => {
+                    this.handleKeyDown(e, index, 4);
+                  }}
                   disabled={this.state.matrix[index][4]}
                   onChange={(e) => {
                     this.handlechange(e, index, 4);
